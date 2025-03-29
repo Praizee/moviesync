0975d13dac8a8194d2e91766f3fc8c39b387f76c
Stephen <praiseadeniji2017@gmail.com>
2025-03-30 00:22:34 +0100

    feat: add nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SupabaseProvider } from "@/components/supabase-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SonnerProvider } from "@/components/sonner-provider";
+import NextTopLoader from "nextjs-toploader";
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
 
@@ -109,6 +110,22 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <NextTopLoader
+              color="#3576DF"
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={4}
+              crawl={true}
+              showSpinner={true}
+              easing="ease"
+              speed={200}
+              shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+              // template='<div class="bar" role="bar"><div class="peg"></div></div>
+              // <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+              zIndex={1600}
+              showAtBottom={false}
+            />
+
             {children}
             <SonnerProvider />
           </ThemeProvider>
